51e2659f24537b598d0b0e15b1e86f0be8ad64d1
Rishabh Joshi <56026938+joshir16@users.noreply.github.com>
2026-04-26 23:30:10 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = new Map()
+
+        for(let str of strs){
+            let sorted = str.split("").sort().join("")
+
+            if(!map.has(sorted)){
+                map.set(sorted, [])
+            }
+
+            map.get(sorted).push(str)
+        }
+          return Array.from(map.values())
+    }
+}
